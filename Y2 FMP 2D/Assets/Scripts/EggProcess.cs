d4fa099ca9248dd857566b36c28877bd61f06f51
CharlotteBuchan <530240@chrome.escg.ac.uk>
2025-04-29 16:18:09 +0100

    EGG PULSING

--- Y2 FMP 2D/Assets/Scripts/EggProcess.cs
+++ Y2 FMP 2D/Assets/Scripts/EggProcess.cs
@@ -1,19 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class EggProcess : MonoBehaviour
 {
     private float timer;
+    private bool isDone;
+    [SerializeField] SpriteRenderer pulseOverlay;
+    private Color oldCol;
+    private Color newCol;
     void Start()
     {
-        timer = 60f;
+        timer = 5f;
+        isDone = false;
+
+        oldCol = new Color(1, 1, 1, 0);
+        newCol = new Color(1, 1, 1, 0.5f);
     }
     void Update()
     {
-        timer -= Time.deltaTime;
-
-        if (timer <= 0f)
+        if (isDone == false)
         {
-            Debug.Log("done timer");
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f)
+            {
+                Debug.Log("Donesies..");
+                isDone = true;
+                StartCoroutine(Pulsate(1f));
+            }
         }
+    }
+
+    private IEnumerator Pulsate(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+        float t = 0;
+
+        while (t < 5)
+        {
+            i = 0;
+            rate = 1 / time;
+
+            while (i < 1)
+            {
+                Debug.Log("Start While");
+                i += Time.deltaTime * rate;
+                pulseOverlay.color = Color.Lerp(oldCol, newCol, i);
+                Debug.Log("End While");
+                yield return 0;
+            }
+
+            i = 0;
+            rate = 1 / time;
+
+            while (i < 1)
+            {
+                Debug.Log("Start While");
+                i += Time.deltaTime * rate;
+                pulseOverlay.color = Color.Lerp(newCol, oldCol, i);
+                Debug.Log("End While");
+                yield return 0;
+            }
+
+            t++;
+        }
+
+        Debug.Log("Done");
+    }
+    private IEnumerator PulsateDown(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        while (i < 1)
+        {
+            Debug.Log("Start While");
+            i += Time.deltaTime * rate;
+            pulseOverlay.color = Color.Lerp(newCol, oldCol, i);
+            Debug.Log("End While");
+            yield return 0;
+        }
+        Debug.Log("Done");
+
     }
 }
